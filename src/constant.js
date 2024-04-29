c30593c356dfb6a630c7dc244747e44acd4ab1f4
Kushagra2024 <Kushagrawal202@gmail.com>
2024-04-29 22:03:05 +0530

    add Admin model

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,9 @@
 const _CONSTANTS = {
     DB_NAME: "Bus-Yatri",
+    ROLES: {
+        ADMIN: "ADMIN",
+        USER: "USER",
+    },
 };
 
 const constants = Object.freeze(_CONSTANTS);
---
+++ src/models/admin.model.js
@@ -0,0 +1,63 @@
+const { Schema, model, Types } = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+const { ROLES } = require("../constant");
+
+const adminSchema = new Schema(
+    {
+        fullname: {
+            type: String,
+            required: [true, "admin fullname is required"],
+            validate: [
+                function (value) {
+                    return value.trim().length > 0;
+                },
+                "Fullname cannot be empty",
+            ],
+        },
+        password: {
+            type: String,
+            required: [true, "password is required"],
+            minLength: [6, "password length cannot be less than 6"],
+        },
+        email: {
+            type: String,
+            required: [true, "email is required"],
+            unique: true,
+            lowercase: true,
+            validate: [isEmail, "Please enter a valid email"],
+            index: true,
+        },
+        role: {
+            type: String,
+            enum: [ROLES.ADMIN],
+            default: ROLES.ADMIN,
+        },
+    },
+    { timestamps: true }
+);
+
+// hash password before save or password modification
+adminSchema.pre("save", async function (next) {
+    try {
+        if (!this.isModified("password")) {
+            return next();
+        }
+
+        const salt = bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+
+        return next();
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+// custom method for verifying user password
+adminSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+const Admin = model("Admin", adminSchema);
+
+module.exports = Admin;
